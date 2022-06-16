53aac40689092ef914d29cc367ac1d417500573d
Tyrone Cloma <tyrone.cloma0219@gmail.com>
2022-06-16 14:51:34 -0700

    commit tree 52b17f2eaeadaec5a27a7a392478726978f32705

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.push(name)
+    return copyOfCats;
+}
+
+function prependCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name)
+    return copyOfCats;
+}
+
+function removeLastCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.pop(name)
+    return copyOfCats;
+}
+
+function removeFirstCat(name){
+    const copyOfCats = [...cats];
+    copyOfCats.shift(name)
+    return copyOfCats;
+}
